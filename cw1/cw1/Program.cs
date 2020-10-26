c861de10cd708a64778bad02c9f8fcffd8428bbe
Konrad Pilewski <pilewski.konrad1@gmail.com>
2020-10-26 16:26:50 +0100

    zad 2

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cw1
@@ -6,9 +8,26 @@ namespace cw1
     class Program
     {
         public static async Task Main(string[] args)
-        {
+        {    
+            
+            Console.WriteLine("Start");
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                var matches = regex.Matches(html);
+
+                foreach (var i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Koniec");
         }
     }
 }

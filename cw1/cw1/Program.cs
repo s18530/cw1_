d7b6c254d1a7b6ed6fbfe3c726136e2cdddc6cb6
Konrad Pilewski <pilewski.konrad1@gmail.com>
2020-10-26 02:45:26 +0100

    Pierwszy commit

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+        }
+    }
+}

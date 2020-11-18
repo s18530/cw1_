1e476e1bc3622d0738d748020b1b4583622bfa13
Konrad Pilewski <pilewski.konrad1@gmail.com>
2020-11-18 09:28:04 +0100

    powrót do zad2 w main

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -10,41 +10,26 @@ namespace cw1
     {
         public static async Task Main(string[] args)
         {
-            Console.WriteLine("Podaj adres URL (  https://www.  )");
-               string url = Console.ReadLine();
-               var urlRegex = new Regex(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
-               var urlMatches = urlRegex.Matches(url ?? throw new ArgumentNullException());
-               var httpClient = new HttpClient();
-               var response = await httpClient.GetAsync(url);
-               
-               if (urlMatches.Count == 0)
-               {
-                   throw new ArgumentException();
-               }
-               
-               if (response.IsSuccessStatusCode)
-               {
-                   var html = await response.Content.ReadAsStringAsync();
-                   var emailRegex = new Regex("[a-z0-9]+@[a-z.]+");
-                   var emailMatches = emailRegex.Matches(html);
-                   var uniqeEmailMatches = emailMatches.OfType<Match>().Select(m => m.Value).Distinct();
-                   if (emailMatches.Count == 0)
-                   {
-                       Console.WriteLine("Nie znaleziono adresow email");
-                   }else
-                   {
-                       foreach (var i in uniqeEmailMatches.ToList())
-                       {
-                           Console.WriteLine(i);
-                       }
-                   }
-               }
-               else
-               {
-                   Console.WriteLine("Blad w czasie pobierania strony");
-               }
-               httpClient.Dispose();
-               Console.WriteLine("Koniec");
+            {
+                Console.WriteLine("Start");
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync("https://www.pja.edu.pl");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var html = await response.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                    var matches = regex.Matches(html);
+
+                    foreach (var i in matches)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.WriteLine("Koniec");
+            }
         }
     }
 }
